383801448d80d1608aeb21a4a8486c4c1d47edb8
Marek Ulicny <xulicny@gmail.com>
2025-02-19 22:00:21 +0100

    add translation to "my profile"

--- src/lib/components/organisms/Links.svelte
+++ src/lib/components/organisms/Links.svelte
@@ -1,5 +1,18 @@
+<script>
+	import i18n from '$lib/i18n'
+
+	const t = i18n({
+		cs: {
+			my_profiles: 'Mé profily',
+		},
+		en: {
+			my_profiles: 'My profiles',
+		},
+	})
+</script>
+
 <section>
-	<h2>Mé profily</h2>
+	<h2>{t('my_profiles')}</h2>
 	<ul class="flex list-none gap-2 text-nowrap">
 		<li><a href="https://stackoverflow.com/users/2016383/exmaxx">SO</a></li>
 		<li><a href="https://github.com/exmaxx">Github</a></li>
